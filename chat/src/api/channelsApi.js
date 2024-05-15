e5d4d2fac660a351c2857eb69fc793abfd2f243f
IvanSH-Jr <ivan_parabel@mail.ru>
2024-05-15 13:06:40 +0300

    add messagesAPI by rtk query

--- chat/src/api/channelsApi.js
+++ chat/src/api/channelsApi.js
@@ -1,17 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import routes from '../routes/routes';
+import authHeader from './authHeader';
 
 export const channelsApi = createApi({
   reducerPath: 'channels',
   baseQuery: fetchBaseQuery({
     baseUrl: routes.channels(),
-    prepareHeaders: (Headers, { getState }) => {
-      const { token } = getState().auth;
-      if (token) {
-        Headers.set('Authorization', `Bearer ${token}`);
-      }
-      return Headers;
-    },
+    prepareHeaders: authHeader,
+    tagTypes: ['Channels'],
   }),
   endpoints: (builder) => ({
     getChannels: builder.query({
