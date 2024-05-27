3c7891669c88f0b2b7a8fb7629d78116c81d198b
IvanSH-Jr <ivan_parabel@mail.ru>
2024-05-27 16:00:21 +0300

    feat(handleLogOut): Nav component

--- chat/src/components/NavComponent.jsx
+++ chat/src/components/NavComponent.jsx
@@ -1,14 +1,26 @@
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { setUserAuth } from '../store/slices/authSlice';
+import routes from '../routes/routes';
 
-const NavComponent = () => (
-  <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-    <div className="container">
-      <Link className="navbar-brand" to="/">
-        Hexlet Chat
-      </Link>
-      <button type="button" className="btn btn-primary">Выйти</button>
-    </div>
-  </nav>
-);
+const NavComponent = () => {
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
+  const handleLogOut = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    dispatch(setUserAuth({ token: null, username: null }));
+  };
+  return (
+    <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+      <div className="container">
+        <Link className="navbar-brand" to={routes.chat()}>
+          Hexlet Chat
+        </Link>
+        {token && (<button type="button" className="btn btn-primary" onClick={handleLogOut}>Выйти</button>)}
+      </div>
+    </nav>
+  );
+};
 
 export default NavComponent;
