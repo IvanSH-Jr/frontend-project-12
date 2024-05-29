de129ac89ea60b51c320c241dee231be4c90e0d8
IvanSH-Jr <ivan_parabel@mail.ru>
2024-05-29 15:16:21 +0300

    init rollbar in chat app

--- chat/src/App.jsx
+++ chat/src/App.jsx
@@ -1,9 +1,8 @@
 import {
   BrowserRouter, Routes, Route, Outlet, Navigate, useLocation,
 } from 'react-router-dom';
-import { Provider, useSelector } from 'react-redux';
-import store from './store/index.js';
-
+import { useSelector } from 'react-redux';
+import { Provider, ErrorBoundary } from '@rollbar/react';
 import routes from './routes/routes.js';
 import NotFound from './Pages/NotFound.jsx';
 import Login from './Pages/Login.jsx';
@@ -19,21 +18,30 @@ const PrivateOutlet = () => {
   return token ? <Outlet /> : <Navigate to={routes.login()} />;
 };
 
+const rollbarConfig = {
+  accessToken: process.env.RollBar_Token,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  environment: process.env.RollBar_App_Environment,
+};
+
 const App = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <AppComponent>
-        <NavComponent />
-        <Routes>
-          <Route path={routes.notFound()} element={<NotFound />} />
-          <Route path={routes.chat()} element={<PrivateOutlet />}>
-            <Route path="" element={<Chat />} />
-          </Route>
-          <Route path={routes.login()} element={<Login />} />
-          <Route path={routes.signup()} element={<Signup />} />
-        </Routes>
-      </AppComponent>
-    </BrowserRouter>
+  <Provider config={rollbarConfig}>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <AppComponent>
+          <NavComponent />
+          <Routes>
+            <Route path={routes.notFound()} element={<NotFound />} />
+            <Route path={routes.chat()} element={<PrivateOutlet />}>
+              <Route path="" element={<Chat />} />
+            </Route>
+            <Route path={routes.login()} element={<Login />} />
+            <Route path={routes.signup()} element={<Signup />} />
+          </Routes>
+        </AppComponent>
+      </BrowserRouter>
+    </ErrorBoundary>
   </Provider>
 );
 
