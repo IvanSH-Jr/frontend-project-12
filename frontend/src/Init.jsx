88cc598ade469e70471fd65717afb0bac5af87a5
IvanSH-Jr <ivan_parabel@mail.ru>
2024-06-05 12:16:57 +0300

    fix(init.jsx): remove all hooks from file

--- frontend/src/Init.jsx
+++ frontend/src/Init.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
-import { Provider, useDispatch } from 'react-redux';
-import { I18nextProvider, useTranslation } from 'react-i18next';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
 import { Provider as RollBarProvider, ErrorBoundary } from '@rollbar/react';
 import i18next from 'i18next';
 import filter from 'leo-profanity';
@@ -12,10 +12,8 @@ import App from './components/App.jsx';
 import store from './slices/index.js';
 import resources from './locales/index.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-/* eslint-disable react/destructuring-assignment */
-const Init = async (socket) => {
-  const dispatch = useDispatch();
-  const { t } = useTranslation();
+
+const Init = async ({ connect, on, off }) => {
   const i18n = i18next.createInstance();
   await i18n.init({
     lng: 'ru',
@@ -36,62 +34,59 @@ const Init = async (socket) => {
   filter.loadDictionary('ru');
   filter.loadDictionary('en');
 
-  useEffect(() => {
-    const handleNewMessage = (newMessage) => {
-      dispatch(
-        messagesApi.util.updateQueryData(
-          'getMessages',
-          undefined,
-          (draftMessages) => { draftMessages.push(newMessage); },
-        ),
-      );
-    };
-    const handleNewChannel = (newChannel) => {
-      dispatch(
-        channelsApi.util.updateQueryData(
-          'getChannels',
-          undefined,
-          (draftChannels) => { draftChannels.push(newChannel); },
-        ),
-      );
-      toast.success(t('chat.notify.addChannel'));
-    };
-    const handleRenameChannel = ({ id, name }) => {
-      dispatch(
-        channelsApi.util.updateQueryData(
-          'getChannels',
-          undefined,
-          (draftChannels) => {
-            const channelIndexToUpdate = draftChannels.findIndex((channel) => channel.id === id);
-            const link = draftChannels;
-            link[channelIndexToUpdate].name = name;
-          },
-        ),
-      );
-      toast.success(t('chat.notify.renameChannel'));
-    };
-    const handleDeleteChannel = ({ id }) => {
-      dispatch(
-        channelsApi.util.updateQueryData(
-          'getChannels',
-          undefined,
-          (draft) => draft.filter((channel) => channel.id !== id),
-        ),
-      );
-      toast.success(t('chat.notify.removeChannel'));
-    };
-    socket.connect();
-    socket.on('newMessage', handleNewMessage);
-    socket.on('newChannel', handleNewChannel);
-    socket.on('renameChannel', handleRenameChannel);
-    socket.on('removeChannel', handleDeleteChannel);
-    return () => {
-      socket.off('newMessage');
-      socket.off('newChannel');
-      socket.off('renameChannel');
-      socket.off('removeChannel');
-    };
-  }, [dispatch, socket, t]);
+  const handleNewMessage = (newMessage) => {
+    store.dispatch(
+      messagesApi.util.updateQueryData(
+        'getMessages',
+        undefined,
+        (draftMessages) => { draftMessages.push(newMessage); },
+      ),
+    );
+    off('newMessage');
+  };
+  const handleNewChannel = (newChannel) => {
+    store.dispatch(
+      channelsApi.util.updateQueryData(
+        'getChannels',
+        undefined,
+        (draftChannels) => { draftChannels.push(newChannel); },
+      ),
+    );
+    toast.success(i18n.t('chat.notify.addChannel'));
+    off('newChannel');
+  };
+  const handleRenameChannel = ({ id, name }) => {
+    store.dispatch(
+      channelsApi.util.updateQueryData(
+        'getChannels',
+        undefined,
+        (draftChannels) => {
+          const channelIndexToUpdate = draftChannels.findIndex((channel) => channel.id === id);
+          const link = draftChannels;
+          link[channelIndexToUpdate].name = name;
+        },
+      ),
+    );
+    toast.success(i18n.t('chat.notify.renameChannel'));
+    off('renameChannel');
+  };
+  const handleDeleteChannel = ({ id }) => {
+    store.dispatch(
+      channelsApi.util.updateQueryData(
+        'getChannels',
+        undefined,
+        (draft) => draft.filter((channel) => channel.id !== id),
+      ),
+    );
+    toast.success(i18n.t('chat.notify.removeChannel'));
+    off('removeChannel');
+  };
+  connect();
+  on('newMessage', handleNewMessage);
+  on('newChannel', handleNewChannel);
+  on('renameChannel', handleRenameChannel);
+  on('removeChannel', handleDeleteChannel);
+
   return (
     <Provider store={store}>
       <React.StrictMode>
